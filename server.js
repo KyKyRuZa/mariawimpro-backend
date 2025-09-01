224c15dac10d37815031132270e041f93b801fb0
KyKyRuZa <ituhvatullin8@gmail.com>
2025-09-01 13:56:46 +0300

    update cors for prod

--- server.js
+++ server.js
@@ -46,7 +46,15 @@ const morganMiddleware = morgan((tokens, req, res) => {
   stream: { write: (message) => logger.info(message.trim()) }
 });
 
-
+app.use(cors({
+  origin: [
+    'http://95.81.118.136',
+    'http://mariaswimpro.ru',
+    'https://mariaswimpro.ru',
+    'http://localhost:5173'
+  ],
+  credentials: true
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(morganMiddleware);
